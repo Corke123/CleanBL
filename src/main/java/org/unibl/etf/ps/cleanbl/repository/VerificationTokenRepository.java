5a848f3cae64df55a8bc607220a71f6f33519b42
Corke123 <bojancoric996@gmail.com>
2021-01-17 14:32:26 +0100

    Set authentication manager and implement UserDetailsService

--- src/main/java/org/unibl/etf/ps/cleanbl/repository/VerificationTokenRepository.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/repository/VerificationTokenRepository.java
@@ -1,10 +1,12 @@
 package org.unibl.etf.ps.cleanbl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.unibl.etf.ps.cleanbl.model.VerificationToken;
 
 import java.util.Optional;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
 }
---
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package org.unibl.etf.ps.cleanbl.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.unibl.etf.ps.cleanbl.model.Permission;
+import org.unibl.etf.ps.cleanbl.model.Role;
+import org.unibl.etf.ps.cleanbl.model.User;
+import org.unibl.etf.ps.cleanbl.repository.UserRepository;
+
+import java.util.*;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("There is no user with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), true,
+                true, true, true,
+                getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        Set<SimpleGrantedAuthority> authoritySet = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            for (Permission permission : role.getPermissions()) {
+                authoritySet.add(new SimpleGrantedAuthority(permission.getName()));
+            }
+            authoritySet.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        }
+        return authoritySet;
+    }
+}
