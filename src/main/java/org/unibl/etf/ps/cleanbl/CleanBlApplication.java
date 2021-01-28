b623410394626e80ab9f5f946bb2ef638b895386
Corke123 <bojancoric996@gmail.com>
2021-01-28 13:42:02 +0100

    Expose API using Swagger and springfox

--- src/main/java/org/unibl/etf/ps/cleanbl/CleanBlApplication.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/CleanBlApplication.java
@@ -2,10 +2,13 @@ package org.unibl.etf.ps.cleanbl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.unibl.etf.ps.cleanbl.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class CleanBlApplication {
 
 	public static void main(String[] args) {
