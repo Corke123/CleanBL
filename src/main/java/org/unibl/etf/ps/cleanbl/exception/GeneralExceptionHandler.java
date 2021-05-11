ea88f5d6146eb91440185996975a8667be8f76ef
Corke123 <bojancoric996@gmail.com>
2021-05-11 23:37:19 +0200

    Remove exception handlers

--- src/main/java/org/unibl/etf/ps/cleanbl/exception/GeneralExceptionHandler.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/exception/GeneralExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.unibl.etf.ps.cleanbl.exception;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,31 +16,5 @@ public class GeneralExceptionHandler {
     public ResponseEntity<Object> handleNotFoundExceptions(ReportNotFoundException e) {
         log.info(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(value = {
-            EmailTakenException.class,
-            UsernameTakenException.class
-    })
-    public ResponseEntity<Object> handleUsernameAndEmailConflicts(IllegalArgumentException e) {
-        log.info(e.getMessage());
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(value = {
-            JWTVerificationException.class,
-            VerificationTokenException.class
-    })
-    public ResponseEntity<Object> invalidLogin(RuntimeException e) {
-        log.info(e.getMessage());
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
-    }
-
-    @ExceptionHandler(value = {
-            RecordNotFoundException.class
-    })
-    public ResponseEntity<Object> handleInternalExceptions(RecordNotFoundException e) {
-        log.info(e.getMessage());
-        return new ResponseEntity<>("A problem occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
