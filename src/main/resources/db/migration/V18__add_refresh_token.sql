1c5f7fc7b28501bf2f8e889fc368a76d2bd25d01
Corke123 <bojancoric996@gmail.com>
2021-06-04 14:28:43 +0200

    Remove unused code

--- src/main/resources/db/migration/V18__add_refresh_token.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `clean_bl`.`RefreshToken` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `createdDate` DATETIME NOT NULL,
-  `token` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
