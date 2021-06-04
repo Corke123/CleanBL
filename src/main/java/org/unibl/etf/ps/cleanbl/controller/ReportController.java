5d059b925b58fad3614bc7deac32b6ac4343cf59
Corke123 <bojancoric996@gmail.com>
2021-06-04 21:37:15 +0200

    Refactor ReportService and ReportController

--- src/main/java/org/unibl/etf/ps/cleanbl/controller/ReportController.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/controller/ReportController.java
@@ -1,6 +1,7 @@
 package org.unibl.etf.ps.cleanbl.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,73 +9,97 @@ import org.unibl.etf.ps.cleanbl.dto.CommentDTO;
 import org.unibl.etf.ps.cleanbl.dto.CommentRequest;
 import org.unibl.etf.ps.cleanbl.dto.ReportRequest;
 import org.unibl.etf.ps.cleanbl.dto.ReportResponse;
-import org.unibl.etf.ps.cleanbl.exception.ReportNotFoundException;
+import org.unibl.etf.ps.cleanbl.mapper.CommentMapper;
+import org.unibl.etf.ps.cleanbl.mapper.ReportMapper;
+import org.unibl.etf.ps.cleanbl.model.Comment;
+import org.unibl.etf.ps.cleanbl.model.Report;
 import org.unibl.etf.ps.cleanbl.service.ReportService;
+import org.unibl.etf.ps.cleanbl.service.UserService;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/v1/reports")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ReportController {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     private final ReportService reportService;
+    private final ReportMapper reportMapper;
+    private final CommentMapper commentMapper;
+    private final UserService userService;
 
     @GetMapping
-    public List<ReportResponse> getAllReports() {
-        return reportService.getAllReports();
+    public ResponseEntity<List<ReportResponse>> getAllReports() {
+        return ResponseEntity.ok(reportService.getAllReports().stream()
+                .map(this::createReportResponseFromReport)
+                .collect(Collectors.toList()));
     }
 
     @PostMapping
-    public ReportResponse uploadReport(@Valid @RequestBody ReportRequest reportRequest) {
-        return reportService.saveReport(reportRequest);
+    public ResponseEntity<ReportResponse> uploadReport(@Valid @RequestBody ReportRequest reportRequest) {
+        Report savedReport = reportService.saveReport(reportRequest);
+        return ResponseEntity.created(URI.create("/api/v1/reports/" + savedReport.getId()))
+                .body(createReportResponseFromReport(savedReport));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ReportResponse> getReport(@PathVariable Long id) {
-        try {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(reportService.getReport(id));
-        } catch (ReportNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return reportService.getReport(id)
+                .map(report -> ResponseEntity.ok(createReportResponseFromReport(report)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Object> deleteReport(@PathVariable("id") Long id) {
-        try {
-            if (reportService.deleteReport(id)) {
-                return ResponseEntity.status(HttpStatus.OK).build();
-            } else {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
-        } catch (ReportNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return reportService.getReport(id)
+                .map(report -> {
+                    if (userService.isLoggedInUserDepartmentOfficer() || report.canUserEditReport(userService.getCurrentlyLoggedInUser())) {
+                        reportService.deleteReport(report);
+                        return ResponseEntity.ok().build();
+                    } else {
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+                    }
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<Object> updateReport(@PathVariable("id") Long id, @Valid @RequestBody ReportRequest reportRequest) {
-        try {
-            if (reportService.updateReport(id, reportRequest)) {
-                return ResponseEntity.status(HttpStatus.OK).build();
-            } else {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
-        } catch (ReportNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return reportService.getReport(id)
+                .map(report -> {
+                    if (userService.isLoggedInUserDepartmentOfficer() || report.canUserEditReport(userService.getCurrentlyLoggedInUser())) {
+                        reportService.updateReport(report, reportRequest);
+                        return ResponseEntity.ok().build();
+                    } else {
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+                    }
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{reportId}/comments/")
-    public List<CommentDTO> getComments(@PathVariable("reportId") Long reportId) {
-        return reportService.getCommentsForReport(reportId);
+    public ResponseEntity<List<CommentDTO>> getComments(@PathVariable("reportId") Long reportId) {
+        return ResponseEntity.ok(reportService.getCommentsForReport(reportId).stream()
+                .map(commentMapper::toDTO)
+                .collect(Collectors.toList()));
     }
 
     @PostMapping("/{reportId}/comments/")
-    public CommentDTO addComment(@PathVariable("reportId") Long reportId, @RequestBody @Valid CommentRequest commentRequest) {
-        return reportService.addComment(reportId, commentRequest);
+    public ResponseEntity<CommentDTO> addComment(@PathVariable("reportId") Long reportId, @RequestBody @Valid CommentRequest commentRequest) {
+        Comment saved = reportService.addComment(reportId, commentRequest);
+        return ResponseEntity.created(URI.create("/api/v1/reports/" + reportId + "/comments/" + saved.getId()))
+                .body(commentMapper.toDTO(saved));
+    }
+
+    private ReportResponse createReportResponseFromReport(Report report) {
+        ReportResponse response = reportMapper.reportToReportResponse(report);
+        response.setBase64Image(report.encodeImage(uploadDir));
+        return response;
     }
 }
--- src/main/java/org/unibl/etf/ps/cleanbl/service/ReportService.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/ReportService.java
@@ -1,21 +1,22 @@
 package org.unibl.etf.ps.cleanbl.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.unibl.etf.ps.cleanbl.dto.CommentDTO;
 import org.unibl.etf.ps.cleanbl.dto.CommentRequest;
 import org.unibl.etf.ps.cleanbl.dto.ReportRequest;
 import org.unibl.etf.ps.cleanbl.dto.ReportResponse;
 import org.unibl.etf.ps.cleanbl.exception.RecordNotFoundException;
 import org.unibl.etf.ps.cleanbl.exception.ReportNotFoundException;
-import org.unibl.etf.ps.cleanbl.model.*;
-import org.unibl.etf.ps.cleanbl.repository.*;
+import org.unibl.etf.ps.cleanbl.mapper.ReportMapper;
+import org.unibl.etf.ps.cleanbl.model.Comment;
+import org.unibl.etf.ps.cleanbl.model.EndUser;
+import org.unibl.etf.ps.cleanbl.model.Report;
+import org.unibl.etf.ps.cleanbl.repository.CommentRepository;
+import org.unibl.etf.ps.cleanbl.repository.ReportRepository;
+import org.unibl.etf.ps.cleanbl.repository.UserRepository;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,11 +24,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ReportService {
 
     @Value("${file.upload-dir}")
@@ -36,165 +38,105 @@ public class ReportService {
     private static final String IMAGE_EXTENSION = ".jpg";
 
     private final ReportRepository reportRepository;
-    private final ReportStatusRepository reportStatusRepository;
-    private final DepartmentRepository departmentRepository;
-    private final PartOfTheCityRepository partOfTheCityRepository;
-    private final StreetRepository streetRepository;
+    private final ReportStatusService reportStatusService;
+    private final DepartmentService departmentService;
+    private final PartOfTheCityService partOfTheCityService;
+    private final StreetService streetService;
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
-
-    @Autowired
-    public ReportService(ReportRepository reportRepository, ReportStatusRepository reportStatusRepository,
-                         DepartmentRepository departmentRepository, PartOfTheCityRepository partOfTheCityRepository,
-                         StreetRepository streetRepository, UserRepository userRepository,
-                         CommentRepository commentRepository) {
-        this.reportRepository = reportRepository;
-        this.reportStatusRepository = reportStatusRepository;
-        this.departmentRepository = departmentRepository;
-        this.partOfTheCityRepository = partOfTheCityRepository;
-        this.streetRepository = streetRepository;
-        this.userRepository = userRepository;
-        this.commentRepository = commentRepository;
-    }
-
+    private final UserService userService;
+    private final ReportMapper reportMapper;
 
     @Transactional
-    public ReportResponse saveReport(ReportRequest reportRequest) {
-        String username = ((org.springframework.security.core.userdetails.User)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        EndUser user = (EndUser) userRepository.findByUsername(username)
+    public Report saveReport(ReportRequest reportRequest) {
+        log.info("Saving new report");
+
+        Report report = Report.builder()
+                .endUser(userService.getEndUserByUsername(userService.getCurrentlyLoggedInUser().getUsername()))
+                .description(reportRequest.getDescription())
+                .imagePath(UUID.randomUUID() + IMAGE_EXTENSION)
+                .reportStatus(reportStatusService.getSentStatus())
+                .street(streetService.getByNameAndPartOfTheCity(reportRequest.getStreet(),
+                        partOfTheCityService.getPartOfTheCityByName(reportRequest.getPartOfTheCity())))
+                .department(departmentService.getByName(reportRequest.getDepartmentName()))
+                .build();
+
+        Report saved = reportRepository.save(report);
+
+        saveImage(report.getImagePath(), reportRequest.getBase64Image());
+
+
+        return saved;
+    }
+
+    public List<Report> getAllReports() {
+        log.info("Getting all reports");
+        return reportRepository.findAll();
+    }
+
+    public Optional<Report> getReport(Long id) throws ReportNotFoundException {
+        log.info("Getting report with id: " + id);
+        return reportRepository.findById(id);
+    }
+
+    public void deleteReport(Report report) throws ReportNotFoundException {
+        log.info("Delete report with id: " + report.getId());
+        deleteImage(report.getImagePath());
+        reportRepository.delete(report);
+    }
+
+    @Transactional
+    public void updateReport(Report report, ReportRequest reportRequest) throws ReportNotFoundException {
+        log.info("Updating report with id: " + report.getId());
+        report.setDepartment(departmentService.getByName(reportRequest.getDepartmentName()));
+        report.setDescription(reportRequest.getDescription());
+        report.setStreet(streetService.getByNameAndPartOfTheCity(reportRequest.getStreet(),
+                partOfTheCityService.getPartOfTheCityByName(reportRequest.getPartOfTheCity())));
+
+        reportRepository.save(report);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Comment> getCommentsForReport(Long reportId) throws ReportNotFoundException {
+        log.info("Getting comments for report with id: " + reportId);
+        Report report = reportRepository.findById(reportId).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + reportId));
+        return commentRepository.findByReport(report);
+    }
+
+    @Transactional
+    public Comment addComment(Long reportId, CommentRequest commentRequest) throws ReportNotFoundException {
+        log.info("Adding new comment for report with id: " + reportId);
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() -> new ReportNotFoundException("There is " + "no report with id: " + reportId));
+        String username = userService.getCurrentlyLoggedInUser().getUsername();
+        EndUser endUser = (EndUser) userRepository.findByUsername(username)
                 .orElseThrow(() -> new RecordNotFoundException("There is no user with username: " + username));
-        Department department = departmentRepository.findByName(reportRequest.getDepartmentName())
-                .orElseThrow(() -> new RecordNotFoundException("Unable to find department with name: " + reportRequest.getDepartmentName()));
-        PartOfTheCity partOfTheCity = partOfTheCityRepository.findByName(reportRequest.getPartOfTheCity())
-                .orElseThrow(() -> new RecordNotFoundException("Unable to find part of the city with name: " + reportRequest.getPartOfTheCity()));
-        Street street = streetRepository.findByNameAndPartOfTheCity(reportRequest.getStreet(), partOfTheCity)
-                .orElseThrow(() -> new RecordNotFoundException("Unable to find street with name: " + reportRequest.getStreet()));
-        ReportStatus reportStatus = reportStatusRepository.findByName("received")
-                .orElseThrow(() -> new RecordNotFoundException("Unable to find report status with name received"));
+        Comment comment = new Comment(report, commentRequest.getContent(), endUser);
+        return commentRepository.save(comment);
+    }
 
-        String imageName = UUID.randomUUID().toString() + IMAGE_EXTENSION;
-
+    private void saveImage(String imageName, String base64Image) {
         Path path = Paths.get(uploadDir + imageName);
         try {
-            Files.write(path, Base64.getDecoder().decode(reportRequest.getBase64Image()));
+            Files.write(path, Base64.getDecoder().decode(base64Image));
             log.debug("Image saved to: " + uploadDir + imageName);
         } catch (IOException e) {
             log.info("Unable to save image on path: " + uploadDir + imageName);
         }
-
-        Report report = new Report(user, reportRequest.getDescription(), imageName,
-                reportStatus,  street, department);
-
-        Report saved = reportRepository.save(report);
-
-        return new ReportResponse(saved.getId(),
-                saved.getEndUser().getUsername(),
-                saved.getDescription(),
-                saved.getCreatedAt(),
-                saved.getReportStatus().getName(),
-                saved.getStreet().getName() + " - " + saved.getStreet().getPartOfTheCity().getName(),
-                saved.getDepartment().getName(),
-                reportRequest.getBase64Image());
     }
 
-    @Transactional(readOnly = true)
-    public List<ReportResponse> getAllReports() {
-        return reportRepository.findAll().stream()
-                .map(r ->
-                    new ReportResponse(
-                        r.getId(),
-                        r.getEndUser().getUsername(),
-                        r.getDescription(),
-                        r.getCreatedAt(),
-                        r.getReportStatus().getName(),
-                        r.getStreet().getName() + " - " + r.getStreet().getPartOfTheCity().getName(),
-                        r.getDepartment().getName(),
-                        r.encodeImage(uploadDir))
-                    )
-                .collect(Collectors.toList());
-    }
-
-    @Transactional(readOnly = true)
-    public ReportResponse getReport(Long id) throws ReportNotFoundException {
-        Report report = reportRepository.findById(id).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + id));
-
-        return new ReportResponse(report.getId(), report.getEndUser().getUsername(), report.getDescription(),
-                report.getCreatedAt(), report.getReportStatus().getName(),
-                report.getStreet().getName() + " - " + report.getStreet().getPartOfTheCity().getName(),
-                report.getDepartment().getName(),
-                report.encodeImage(uploadDir));
-    }
-
-    @Transactional
-    public boolean deleteReport(Long id) throws ReportNotFoundException {
-        String username = ((org.springframework.security.core.userdetails.User)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        boolean isDepartmentOfficer = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAuthorities()
-                .stream().map(GrantedAuthority::getAuthority)
-                .anyMatch(s -> s.equals("ROLE_DepartmentOfficer"));
-        Report report = reportRepository.findById(id).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + id));
-        if (isDepartmentOfficer || report.getEndUser().getUsername().equals(username)) {
-            reportRepository.delete(report);
-            return true;
-        } else {
-            return false;
+    private void deleteImage(String image) {
+        try {
+            Files.delete(Path.of(uploadDir + image));
+            log.info("Deleting file: + " + image);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    @Transactional
-    public boolean updateReport(Long id, ReportRequest reportRequest) throws ReportNotFoundException {
-        String username = ((org.springframework.security.core.userdetails.User)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        boolean isDepartmentOfficer = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAuthorities()
-                .stream().map(GrantedAuthority::getAuthority)
-                .anyMatch(s -> s.equals("ROLE_DepartmentOfficer"));
-        Report report = reportRepository.findById(id).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + id));
-        if (isDepartmentOfficer || report.getEndUser().getUsername().equals(username)) {
-            Department department = departmentRepository.findByName(reportRequest.getDepartmentName())
-                    .orElseThrow(() -> new RecordNotFoundException("Unable to find department with name: "
-                    + reportRequest.getDepartmentName()));
-            PartOfTheCity partOfTheCity = partOfTheCityRepository.findByName(reportRequest.getPartOfTheCity())
-                    .orElseThrow(() -> new RecordNotFoundException("Unable to find part of the city with name: " + reportRequest.getPartOfTheCity()));
-            Street street = streetRepository.findByNameAndPartOfTheCity(reportRequest.getStreet(), partOfTheCity)
-                    .orElseThrow(() -> new RecordNotFoundException("Unable to find street with name " + reportRequest.getStreet() + " in the " + partOfTheCity.getName()));
-
-            String imageName = UUID.randomUUID().toString() + IMAGE_EXTENSION;
-            Path path = Paths.get(uploadDir + imageName);
-            try {
-                Files.write(path, Base64.getDecoder().decode(reportRequest.getBase64Image()));
-                log.debug("Image saved to: " + uploadDir + imageName);
-            } catch (IOException e) {
-                log.info("Unable to save image on path: " + uploadDir + imageName);
-            }
-
-            report.setDepartment(department);
-            report.setDescription(reportRequest.getDescription());
-            report.setStreet(street);
-            report.setImagePath(imageName);
-
-            reportRepository.save(report);
-            return true;
-        }
-        return false;
-    }
-
-    @Transactional(readOnly = true)
-    public List<CommentDTO> getCommentsForReport(Long reportId) throws ReportNotFoundException {
-        Report report = reportRepository.findById(reportId).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + reportId));
-        return commentRepository.findByReport(report).stream()
-                .map(c -> new CommentDTO(c.getReport().getId(), c.getDateOfPublication(), c.getContent(), c.getEndUser().getUsername()))
-                .collect(Collectors.toList());
-    }
-
-    @Transactional
-    public CommentDTO addComment(Long reportId, CommentRequest commentRequest) throws ReportNotFoundException {
-        Report report = reportRepository.findById(reportId).orElseThrow(() -> new ReportNotFoundException("There is no report with id: " + reportId));
-        String username = ((org.springframework.security.core.userdetails.User)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        EndUser endUser = (EndUser) userRepository.findByUsername(username).orElseThrow(() -> new RecordNotFoundException("There is no user with username: " + username));
-        Comment comment = new Comment(report, commentRequest.getContent(), endUser);
-        commentRepository.save(comment);
-        return new CommentDTO(report.getId(), comment.getDateOfPublication(), comment.getContent(), comment.getEndUser().getUsername());
+    private ReportResponse createReportResponseFromReport(Report report) {
+        ReportResponse response = reportMapper.reportToReportResponse(report);
+        response.setBase64Image(report.encodeImage(uploadDir));
+        return response;
     }
 }
