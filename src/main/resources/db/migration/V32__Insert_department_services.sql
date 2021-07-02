f5b68e8eb2f5188dfaf29231099589ddacd5f593
Corke123 <bojancoric996@gmail.com>
2021-07-02 22:00:07 +0200

    Create script to insert department services

---
+++ src/main/resources/db/migration/V32__Insert_department_services.sql
@@ -0,0 +1,6 @@
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('MG - Mind', '050/280-040', 'mgmind@teol.net', 1);
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('Prijedorputevi a.d.', '052/233-117', 'prijedorputevi@fortisgroup.ba', 1);
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('Čistoća AD', '051/304-477', 'info@cistocabl.com', 2);
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('Vodovod A.D.', '051/212-380', 'info@vodovod-bl.com', 2);
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('Tamaris Company', '051/216-451', 'info@tamariscompany.com', 2);
+INSERT INTO DepartmentService (Name, Phone, Email, DepartmentId) VALUES ('Elektroobnova', '051/394-280', 'info@elektroobnova.com', 2);
