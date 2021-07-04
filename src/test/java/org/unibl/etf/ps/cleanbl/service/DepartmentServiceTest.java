a6dace69edff81e0ba10db215fe8bbcbc4f6f762
Boris22071995 <bvasiljevic21@gmail.com>
2021-07-04 14:19:11 +0200

    Test departmentService

--- src/test/java/org/unibl/etf/ps/cleanbl/service/DepartmentServiceTest.java
+++ src/test/java/org/unibl/etf/ps/cleanbl/service/DepartmentServiceTest.java
@@ -6,11 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.unibl.etf.ps.cleanbl.model.Department;
-import org.unibl.etf.ps.cleanbl.model.Role;
-import org.unibl.etf.ps.cleanbl.model.User;
 import org.unibl.etf.ps.cleanbl.repository.DepartmentRepository;
-import org.unibl.etf.ps.cleanbl.repository.RoleRepository;
-import org.unibl.etf.ps.cleanbl.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,28 +22,24 @@ class DepartmentServiceTest {
     @Mock
     DepartmentRepository departmentRepository;
 
-    @Mock
-    RoleRepository roleRepository;
-
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-        @Test
+
+    @Test
     void getByDepartmentName_should_return_department_name() {
-            Department department = new Department();
-            department.setName("Komunalni poslovi");
-            department.setEmail("komunalno@gmail.com");
-            department.setPhone("051/222-222");
-            department.setId(1L);
+        Department department = Department.builder().id(1L).name("Komunalni poslovi").email("komunalno@gmail.com").phone("051/222-222").build();
         when(departmentRepository.findByName("Komunalni poslovi")).thenReturn(Optional.of(department));
         assertEquals(department,departmentService.getByName("Komunalni poslovi"));
     }
 
     @Test
-    void getDepartments_should_return_all_departments () {
-        List<Department> departments = departmentRepository.findAll();
-        assertEquals(departments,departmentService.getDepartments());
+    void getDepartments_should_return_all_departments() {
+        List<Department> departments = Collections.singletonList(
+                 Department.builder().id(1L).name("Komunalni poslovi").email("komunalno@gmail.com").phone("051/222-222").build());
+        when(departmentRepository.findAll()).thenReturn(departments);
+        assertEquals(departments, departmentService.getDepartments());
     }
 
 }
