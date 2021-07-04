e10493066d31d83a8e1f0302435016d0539a30a5
Corke123 <bojancoric996@gmail.com>
2021-07-04 02:23:14 +0200

    Enable fetching department service by name

--- src/main/java/org/unibl/etf/ps/cleanbl/repository/DepartmentServiceRepository.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/repository/DepartmentServiceRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.unibl.etf.ps.cleanbl.model.DepartmentService;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentServiceRepository extends JpaRepository<DepartmentService, Long> {
     List<DepartmentService> findAllByDepartmentName(String departmentName);
+
+    Optional<DepartmentService> findByName(String name);
 }
