d01224b1968a2529a6965a76817cfa0e53c81864
Boris22071995 <bvasiljevic21@gmail.com>
2021-07-06 18:09:57 +0200

    Test that method throws exception

--- src/test/java/org/unibl/etf/ps/cleanbl/service/RoleServiceTest.java
+++ src/test/java/org/unibl/etf/ps/cleanbl/service/RoleServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.unibl.etf.ps.cleanbl.exception.RecordNotFoundException;
 import org.unibl.etf.ps.cleanbl.model.Role;
 import org.unibl.etf.ps.cleanbl.repository.RoleRepository;
 
@@ -37,6 +38,18 @@ class RoleServiceTest {
         Role role = Role.builder().name("DepartmentOfficer").id(1L).build();
         when(roleRepository.findByName("DepartmentOfficer")).thenReturn(Optional.of(role));
         assertEquals(role,roleService.getDepartmentOfficerRole());
+    }
+
+    @Test
+    void getUserRole_should_throws_exception_when_role_not_exist() {
+        when(roleRepository.findByName("User")).thenReturn(Optional.empty());
+        assertThrows(RecordNotFoundException.class, () -> roleService.getUserRole());
+    }
+
+    @Test
+    void getDepartmentOfficerRole_should_throws_exception_when_role_not_exist() {
+        when(roleRepository.findByName("DepartmentOfficer")).thenReturn(Optional.empty());
+        assertThrows(RecordNotFoundException.class, () -> roleService.getDepartmentOfficerRole());
     }
 
 }
