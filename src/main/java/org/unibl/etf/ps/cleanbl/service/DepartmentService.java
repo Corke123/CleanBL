f5bd164460ad4bfeb8cb05ccf61c66ea45f7353d
dario1338 <dario.petric1338@gmail.com>
2021-07-14 23:42:51 +0200

    Fix conflict with master

--- src/main/java/org/unibl/etf/ps/cleanbl/service/DepartmentService.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/DepartmentService.java
@@ -1,30 +1,11 @@
 package org.unibl.etf.ps.cleanbl.service;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.unibl.etf.ps.cleanbl.exception.RecordNotFoundException;
 import org.unibl.etf.ps.cleanbl.model.Department;
-import org.unibl.etf.ps.cleanbl.repository.DepartmentRepository;
 
 import java.util.List;
 
-@Service
-@AllArgsConstructor
-@Slf4j
-public class DepartmentService {
+public interface DepartmentService {
 
-    private final DepartmentRepository departmentRepository;
-
-    public List<Department> getDepartments() {
-        log.info("Getting all departments");
-        return departmentRepository.findAll();
-    }
-
-    public Department getByName(String name) {
-        log.info("Find department with name: " + name);
-        return departmentRepository.findByName(name)
-                .orElseThrow(() -> new RecordNotFoundException("Unable to find department with name: " + name));
-    }
-
+    List<Department> getDepartments();
+    Department getByName(String name);
 }
