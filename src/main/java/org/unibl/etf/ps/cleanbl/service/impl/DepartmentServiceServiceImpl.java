59e3b201e4f796ca2c7361499b79c5ab72edb16b
dario1338 <dario.petric1338@gmail.com>
2021-07-14 11:39:55 +0200

    Create access interface to hide implementation

---
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/impl/DepartmentServiceServiceImpl.java
@@ -0,0 +1,34 @@
+package org.unibl.etf.ps.cleanbl.service.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.unibl.etf.ps.cleanbl.exception.RecordNotFoundException;
+import org.unibl.etf.ps.cleanbl.model.DepartmentService;
+import org.unibl.etf.ps.cleanbl.repository.DepartmentServiceRepository;
+import org.unibl.etf.ps.cleanbl.service.DepartmentServiceService;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class DepartmentServiceServiceImpl implements DepartmentServiceService {
+    private final UserService userService;
+
+    private final DepartmentServiceRepository departmentServiceRepository;
+
+    @Override
+    public List<DepartmentService> getDepartmentServicesForUsersDepartment() {
+        return departmentServiceRepository.findAllByDepartmentName(
+                userService.getUserByUsername(userService.getCurrentlyLoggedInUser().getUsername())
+                        .getDepartment().getName());
+    }
+
+    @Override
+    public DepartmentService getByName(String name) {
+        log.info("Get department service by name: " + name);
+        return departmentServiceRepository.findByName(name)
+                .orElseThrow(() -> new RecordNotFoundException("Unable to find department service with name: " + name));
+    }
+}
