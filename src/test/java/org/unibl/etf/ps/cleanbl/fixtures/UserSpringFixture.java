4ea8ea5398422e5deae7b14ee219318bccf28274
dario1338 <dario.petric1338@gmail.com>
2021-07-21 16:01:28 +0200

    Remove methods createDepartment, createUserStatus, createRole

--- src/test/java/org/unibl/etf/ps/cleanbl/fixtures/UserSpringFixture.java
+++ src/test/java/org/unibl/etf/ps/cleanbl/fixtures/UserSpringFixture.java
@@ -2,20 +2,17 @@ package org.unibl.etf.ps.cleanbl.fixtures;
 
 import org.springframework.security.core.userdetails.User;
 import org.unibl.etf.ps.cleanbl.dto.DepartmentOfficerDTO;
-import org.unibl.etf.ps.cleanbl.model.Department;
-import org.unibl.etf.ps.cleanbl.model.Role;
-import org.unibl.etf.ps.cleanbl.model.UserStatus;
 
 public class UserSpringFixture {
 
     public static User.UserBuilder crateUserSpring() {
-        return                  org.springframework.security.core.userdetails.User.builder()
-                                .username("marko123")
-                                .accountLocked(false)
-                                .accountExpired(false)
-                                .credentialsExpired(false)
-                                .authorities("ROLE_DepartmentOfficer")
-                                .password("marko123");
+        return org.springframework.security.core.userdetails.User.builder()
+                .username("marko123")
+                .accountLocked(false)
+                .accountExpired(false)
+                .credentialsExpired(false)
+                .authorities("ROLE_DepartmentOfficer")
+                .password("marko123");
     }
 
     public static org.unibl.etf.ps.cleanbl.model.User.UserBuilder createUser() {
@@ -27,10 +24,6 @@ public class UserSpringFixture {
                 .email("marko123@gmail.com");
     }
 
-    public static Department.DepartmentBuilder createDepartment() {
-        return Department.builder().id(1L).name("Department");
-    }
-
     public static DepartmentOfficerDTO createDepartmentOfficerDTO() {
         return new DepartmentOfficerDTO(1L,
                 "Marko",
@@ -39,12 +32,5 @@ public class UserSpringFixture {
                 "marko123@gmail.com",
                 "Department 1");
     }
-
-    public static UserStatus.UserStatusBuilder createUserStatus() {
-        return UserStatus.builder().id(1L).name("active");
-    }
-
-    public static Role.RoleBuilder createRole() {
-        return Role.builder().name("DepartmentOfficer");
-    }
 }
+
