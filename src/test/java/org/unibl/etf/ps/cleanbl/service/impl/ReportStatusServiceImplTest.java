5dcd59b68f3fa1a49295d3dd095293a46cd5dbfa
Boris22071995 <bvasiljevic21@gmail.com>
2021-07-27 10:08:00 +0200

    Create test for all methodes

--- src/test/java/org/unibl/etf/ps/cleanbl/service/impl/ReportStatusServiceImplTest.java
+++ src/test/java/org/unibl/etf/ps/cleanbl/service/impl/ReportStatusServiceImplTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.unibl.etf.ps.cleanbl.fixtures.ReportStatusFixture;
 import org.unibl.etf.ps.cleanbl.model.ReportStatus;
 import org.unibl.etf.ps.cleanbl.repository.ReportStatusRepository;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,9 +31,31 @@ class ReportStatusServiceImplTest {
     @Test
     void getAllStatuses_should_return_all_statuses() {
         List<ReportStatus> list = Arrays.asList(
-                ReportStatus.builder().id(1L).name("poslan").build(),
-                ReportStatus.builder().id(2L).name("zavrsen").build());
+                ReportStatusFixture.createSent().build(),
+                ReportStatusFixture.createCompleted().build());
         when(reportStatusRepository.findAll()).thenReturn(list);
         assertEquals(list, reportStatusService.getAllStatuses());
     }
+
+    @Test
+    void getSentStatus_should_return_sent_status() {
+        ReportStatus reportStatus = ReportStatusFixture.createSent().build();
+        when(reportStatusRepository.findByName("poslan")).thenReturn(Optional.of(reportStatus));
+        assertEquals(reportStatus, reportStatusService.getSentStatus());
+    }
+
+    @Test
+    void getInProcessStatus_should_return_in_process_status() {
+        ReportStatus reportStatus = ReportStatusFixture.createInProcess().build();
+        when(reportStatusRepository.findByName("u procesu")).thenReturn(Optional.of(reportStatus));
+        assertEquals(reportStatus, reportStatusService.getInProcessStatus());
+    }
+
+    @Test
+    void getCompletedStatus_should_return_completed_status() {
+        ReportStatus reportStatus = ReportStatusFixture.createCompleted().build();
+        when(reportStatusRepository.findByName("zavrsen")).thenReturn(Optional.of(reportStatus));
+        assertEquals(reportStatus, reportStatusService.getCompletedStatus());
+    }
+
 }
