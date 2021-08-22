b47ce4c40320524b59eea0b124c9f54efb366c3b
Boris22071995 <bvasiljevic21@gmail.com>
2021-08-22 18:14:36 +0200

    Use year as request param

--- src/main/java/org/unibl/etf/ps/cleanbl/controller/StatisticsController.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/controller/StatisticsController.java
@@ -2,10 +2,7 @@ package org.unibl.etf.ps.cleanbl.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.unibl.etf.ps.cleanbl.dto.StatisticsDTO;
 import org.unibl.etf.ps.cleanbl.service.StatisticsService;
 
@@ -18,8 +15,8 @@ public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
-    @GetMapping("/{year}")
-    public ResponseEntity<List<StatisticsDTO>> getStatistics(@PathVariable("year") Integer year) {
+    @GetMapping
+    public ResponseEntity<List<StatisticsDTO>> getStatistics(@RequestParam Integer year) {
         return ResponseEntity.ok(statisticsService.getStatistics(year));
     }
 }
