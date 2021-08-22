efe92a65a10c7f7314f527b46c147926a095964e
Kristina Maric <kia.maric97@gmail.com>
2021-08-22 16:01:00 +0200

    Use template to send emails

--- src/main/java/org/unibl/etf/ps/cleanbl/service/EmailService.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/EmailService.java
@@ -1,5 +1,8 @@
 package org.unibl.etf.ps.cleanbl.service;
 
+import java.util.Map;
+
 public interface EmailService {
     void sendMessage(String to, String subject, String text);
+    void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel);
 }
