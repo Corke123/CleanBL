04d47f278a62f6484000b1593c531b56afa25153
dario1338 <dario.petric1338@gmail.com>
2021-09-07 21:03:28 +0200

    Find average grade by department service and year

--- src/main/java/org/unibl/etf/ps/cleanbl/service/StatisticsService.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/StatisticsService.java
@@ -1,10 +1,12 @@
 package org.unibl.etf.ps.cleanbl.service;
 
 import org.unibl.etf.ps.cleanbl.dto.StatisticsDTO;
+import org.unibl.etf.ps.cleanbl.model.DepartmentService;
 
 import java.util.List;
 
 public interface StatisticsService {
 
     List<StatisticsDTO> getStatistics(Integer year);
+    public StatisticsDTO getStatisticsByDepartment(Integer year, DepartmentService departmentService);
 }
--- src/main/java/org/unibl/etf/ps/cleanbl/service/impl/StatisticsServiceImpl.java
+++ src/main/java/org/unibl/etf/ps/cleanbl/service/impl/StatisticsServiceImpl.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.unibl.etf.ps.cleanbl.dto.StatisticsDTO;
+import org.unibl.etf.ps.cleanbl.model.DepartmentService;
+import org.unibl.etf.ps.cleanbl.model.Evaluates;
 import org.unibl.etf.ps.cleanbl.model.ReportStatus;
+import org.unibl.etf.ps.cleanbl.repository.EvaluatesRepository;
 import org.unibl.etf.ps.cleanbl.repository.ReportRepository;
 import org.unibl.etf.ps.cleanbl.service.ReportStatusService;
 import org.unibl.etf.ps.cleanbl.service.StatisticsService;
@@ -13,6 +16,7 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 
 @Service
 @Slf4j
@@ -21,6 +25,7 @@ public class StatisticsServiceImpl implements StatisticsService {
 
     private final ReportRepository reportRepository;
     private final ReportStatusService reportStatusService;
+    private final EvaluatesRepository evaluatesRepository;
 
     @Override
     public List<StatisticsDTO> getStatistics(Integer year) {
@@ -35,11 +40,41 @@ public class StatisticsServiceImpl implements StatisticsService {
         return statistics;
     }
 
-    private List<Integer> getReportCountByReportStatus(ReportStatus reportStatus, Integer year) {
-        List<Integer> values = new ArrayList<>();
+
+    @Override
+    public StatisticsDTO getStatisticsByDepartment(Integer year, DepartmentService departmentService) {
+        StatisticsDTO statistics = new StatisticsDTO();
+
+        statistics.setName(departmentService.getName());
+        statistics.setData(getEvaluatesAverageByDepartmentService(year, departmentService.getId()));
+
+        return statistics;
+    }
+
+    private List<Double> getEvaluatesAverageByDepartmentService(Integer year, Long departmentServiceId) {
+        List<Double> values = new ArrayList<>();
+        for (Month month : Month.values()) {
+            OptionalDouble avg = evaluatesRepository.findAll()
+                            .stream()
+                            .filter(e -> e.getId().getDepartmentService().getId().equals(departmentServiceId)
+                                    && e.getGradedAt().getMonth().equals(month)
+                                    && e.getGradedAt().getYear() == year)
+                            .mapToDouble(Evaluates::getGrade)
+                            .average();
+            if (avg.isPresent()) {
+                values.add(avg.getAsDouble());
+            } else {
+                values.add(0.0);
+            }
+        }
+        return  values;
+    }
+
+    private List<Double> getReportCountByReportStatus(ReportStatus reportStatus, Integer year) {
+        List<Double> values = new ArrayList<>();
 
         for(Month month : Month.values()) {
-            values.add((int) reportRepository.findAll()
+            values.add((double) reportRepository.findAll()
                     .stream()
                     .filter(r -> r.getReportStatus().equals(reportStatus)
                             && r.getCreatedAt().getMonth().equals(month)
